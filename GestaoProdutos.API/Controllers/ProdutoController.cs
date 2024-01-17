49d544a73047e49ac618c74a5da1e89de4d23d2f
Julio Martins <julio.martins@tuddu.com.br>
2024-01-17 10:25:08 -0300

    commit inicial

---
+++ GestaoProdutos.API/Controllers/ProdutoController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoProdutos.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProdutoController : ControllerBase
+    {
+        private static readonly string[] Produtos = new[]
+        {
+            "Lanterna C3", "Pastilha de freio Ford", "Calota Peugeot 206", "Protetor de Cárter Montana"
+        };
+
+        public ProdutoController()
+        {
+            
+        }
+
+        [HttpGet]
+        public IEnumerable<Produto> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 3).Select(index => new Produto
+            {
+                Descricao = Produtos[rng.Next(Produtos.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ GestaoProdutos.API/Produto.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace GestaoProdutos.API
+{
+    public class Produto
+    {
+        public string Descricao { get; set; }
+    }
+}
